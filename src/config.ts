1a0cb1b6ddd8bbd255d0f851d976c2d8f09c5603
Natto-PKP <wmainvielle@gmail.com>
2022-06-09 00:57:42 +0200

    V 1.0.8 - 🎉 add defaults colors

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+export const colors = <{ [key: string]: `#${string}` }>{
+  cream: '#fff7d4',
+  darkBlue: '#304eb0',
+  darkGrey: '#2c2f33',
+  lightBlue: '#abd8de',
+  lightPink: '#f9cadc',
+  green: '#aace6e',
+  purple: '#be2d8c',
+  white: '#feffff',
+};
+
+export const none = null;
